6a639ef8a572787c25220f31e6ca97fb3f7229ad
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-09-04 12:31:26 -0300

    Segundo commit

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json()) //irá fazer o parse de arquivos JSON
+
+//Rotas de conteúdo público
+app.use('/', express.static('public'));
+
+//Configura o favicon
+app.use('/favicon.ico', express.static('public/images/pc_icon.ico'))
+
+//Rotas de API
+app.get('/api', (req, res) => {
+    res.status(200).json(
+        {
+            "message": 'API Fatec 100% funcional 🖐',
+            "version": '1.0.0'
+        }
+    )
+});
+
+//Rotas de exceção -> deve ser a última!
+app.use(function(req, res) {
+    res.status(404).json({
+        errors: [{
+            "value": `${req.originalUrl}`,
+            "msg": `A rota ${req.originalUrl} não existe nesta API!`,
+            "param": `Invalid route`
+        }]
+    })
+});
+
+app.listen(port, function(){
+    console.log(`📲 Servidor rodando na porta ${port}`)
+});
